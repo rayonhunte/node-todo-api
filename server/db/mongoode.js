e3ad03e643aa08b683d3b8d2ac92c2484cbab304
Rayon Hunte <rayon.hunte@gmail.com>
2018-04-09 17:13:13 -0400

    added mlab

--- server/db/mongoode.js
+++ server/db/mongoode.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise; // set to use default promises
-mongoose.connect('mongodb://localhost:27017/TodoApp'); // connect to local host
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); // connect to local host
 
 
 module.exports = {mongoose}
