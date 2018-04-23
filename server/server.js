f7379ad40ce5e249ba8f5166ed059f631e8450d7
Rayon Hunte <rayon.hunte@gmail.com>
2018-04-22 23:28:40 -0400

    updated server file

--- server/server.js
+++ server/server.js
@@ -1,16 +1,3 @@
-// const env = process.env.NODE_ENV || 'development';
-// console.log('env ******* ', env);
-
-
-// if(env === 'development'){
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-// }else if(env==='test'){
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
-
-
 require('./config/config');
 
 const express = require('express');
